94a17cecc6dc8b2e81dd07f1100fe459f8f5420a
Tsvetomir Penchev <tsvetomir.penchev@mentormate.com>
2023-07-26 17:45:25 +0300

    use promises

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# CoffeeBar
-The CoffeeBar task
+## Buffers - Text to Speech
 
+Create a program that converts text to MP3, using google translate. You don’t need any external libraries.
+It is enough to make a GET request to the google translate URL and process the binary response.
 
-### Install Node modules
+Example:  
+[https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=Beware+of+heard+a+dreadful+word+that+looks+like+beard+and+sounds+like+bird](https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=Beware+of+heard+a+dreadful+word+that+looks+like+beard+and+sounds+like+bird)
+
+## Running the app
 
 ```bash
 npm install
---
+++ src/modules/text-to-speech/index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const querystring = require('querystring');
+
+module.exports = class TextToSpeechConverter {
+  baseUrl = 'https://translate.google.com/translate_tts';
+
+  queryParams = {
+    ie: 'UTF-8',
+    client: 'tw-ob',
+    tl: 'en',
+    q: '',
+  };
+
+  text = '';
+
+  getUrl() {
+    this.queryParams.q = this.text;
+    return `${this.baseUrl}?${querystring.stringify(this.queryParams)}`;
+  }
+
+  getAudioData() {
+    return new Promise((resolve, reject) => {
+      https.get(this.getUrl(), (response) => {
+        const chunks = [];
+
+        if (response.statusCode !== 200) {
+          reject(
+            new Error(
+              `Request failed with status code: ${response.statusCode}`,
+            ),
+          );
+        }
+
+        response.on('data', (chunk) => chunks.push(chunk));
+        response.on('end', () => resolve(Buffer.concat(chunks)));
+        response.on('error', (error) => reject(error));
+      });
+    });
+  }
+
+  getFileName() {
+    const cleanedText = this.text
+      .replace(/[/\\?*:|,\\!"<>]/g, '')
+      .replace(/\s+/g, '-')
+      .slice(0, 50)
+      .toLowerCase();
+
+    const currentDate = new Date();
+    const year = currentDate.getFullYear().toString();
+    const day = currentDate.getDate().toString();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+
+    return `${year}-${month}-${day}-${cleanedText}.mp3`;
+  }
+
+  saveDataToFile(audioData) {
+    return new Promise((resolve, reject) => {
+      const filename = `${path.resolve('output', this.getFileName())}`;
+
+      fs.writeFile(filename, audioData, 'binary', (error) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`${filename} saved`);
+      });
+    });
+  }
+
+  save(text) {
+    this.text = text;
+
+    this.getAudioData()
+      .then((result) => this.saveDataToFile(result))
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error.toString()));
+  }
+};
