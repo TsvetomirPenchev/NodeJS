78e149e3184a653fdd805a04f4232bb53fb04c5d
Tsvetomir Penchev <tsvetomir.penchev@mentormate.com>
2023-08-01 14:47:18 +0300

    wrap into array

--- src/modules/csv-to-json/index.js
+++ src/modules/csv-to-json/index.js
@@ -5,13 +5,16 @@ module.exports = class CsvToJsonTransformer extends Transform {
     super(options);
     this.separator = options?.separator || ',';
     this.headers = null;
+    this.firstChunk = true;
+
+    this.push('[');
   }
 
   _transform(chunk, encoding, callback) {
     const lines = chunk.toString().split('\n');
 
     // If this is the first chunk, extract headers
-    if (!this.headers) {
+    if (this.firstChunk) {
       this.headers = lines.shift().split(this.separator);
     }
 
@@ -25,8 +28,20 @@ module.exports = class CsvToJsonTransformer extends Transform {
         object[this.headers[i]] = values[i];
       }
 
-      this.push(`${JSON.stringify(object)}\n`);
+      if (this.firstChunk) {
+        this.firstChunk = false;
+      } else {
+        this.push(',');
+      }
+
+      this.push(`${JSON.stringify(object)}`);
     });
+
+    callback();
+  }
+
+  _flush(callback) {
+    this.push(']');
 
     callback();
   }
