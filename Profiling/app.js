0a93e081f5d7f64e53787adc297389729a58ab4b
Tsvetomir Penchev <tsvetomir.penchev@mentormate.com>
2023-08-02 19:43:12 +0300

    no cache header added

--- Profiling/app.js
+++ Profiling/app.js
@@ -8,25 +8,19 @@ const users = [];
 // .\ab -k -c 20 -n 300 "http://3.120.206.144:8080/file"
 // NODE_ENV=production node --prof app.js
 
-app.get("/file", (req, res) => {
+app.get("/fileSync", (req, res) => {
   const text = fs.readFileSync("text.txt");
-
+  res.set("Cache-Control", "no-store");
   res.set("Content-Type", "text/html");
   res.send(text);
 });
 
-app.get("/fileStream", (req, res) => {
+app.get("/file", (req, res) => {
   res.set("Content-Type", "text/html");
-
-  const stream = fs.createReadStream("text.txt");
-  stream.pipe(res);
-});
-
-app.get("/fileSync", (req, res) => {
-  res.set("Content-Type", "text/html");
-
-  const stream = fs.createReadStream("text.txt");
-  stream.pipe(res);
+  res.set("Cache-Control", "no-store");
+  fs.readFile("text.txt", (err, data) => {
+    res.send(data);
+  });
 });
 
 app.listen(PORT, function (err) {
