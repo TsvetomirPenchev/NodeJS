5898a7ba04f0cf8136b73f38abf5a872235dd3a0
Tsvetomir Penchev <tsvetomir.penchev@mentormate.com>
2023-08-16 15:43:06 +0300

    profiling

--- Profiling/app.js
+++ Profiling/app.js
@@ -1,25 +1,59 @@
 const express = require("express");
-const fs = require("fs");
+const crypto = require("crypto");
 const app = express();
 const PORT = 8080;
 
+// http://127.0.0.1:8080/newUser?username=matt&password=password
+// .\ab -k -c 50 -n 1000 "http://127.0.0.1:8080/auth?username=matt&password=password"
+// NODE_ENV=production node --prof app2.js
+
 const users = [];
 
-// .\ab -k -c 20 -n 300 "http://3.120.206.144:8080/file"
-// NODE_ENV=production node --prof app.js
+app.get("/newUser", (req, res) => {
+  let username = req.query.username || "";
+  const password = req.query.password || "";
 
-app.get("/fileSync", (req, res) => {
-  const text = fs.readFileSync("text.txt");
-  res.set("Cache-Control", "no-store");
-  res.set("Content-Type", "text/html");
-  res.send(text);
+  username = username.replace(/[!@#$%^&*]/g, "");
+
+  if (!username || !password || users[username]) {
+    return res.sendStatus(400);
+  }
+
+  const salt = crypto.randomBytes(128).toString("base64");
+  const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
+
+  users[username] = { salt, hash };
+
+  res.sendStatus(200);
 });
 
-app.get("/file", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.set("Cache-Control", "no-store");
-  fs.readFile("text.txt", (err, data) => {
-    res.send(data);
+app.get("/auth", (req, res) => {
+  let username = req.query.username || "";
+  const password = req.query.password || "";
+
+  username = username.replace(/[!@#$%^&*]/g, "");
+
+  if (!username || !password || !users[username]) {
+    return res.sendStatus(400);
+  }
+
+  const { salt, hash } = users[username];
+
+  // Sync
+  // const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
+  // if (crypto.timingSafeEqual(hash, encryptHash)) {
+  //   res.sendStatus(200);
+  // } else {
+  //   res.sendStatus(401);
+  // }
+
+  // Async
+  crypto.pbkdf2(password, salt, 10000, 512, "sha512", (err, encryptHash) => {
+    if (hash.toString() === encryptHash.toString()) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(401);
+    }
   });
 });
 
